51ec32408a795d3a8c8d466a4ca682cae5a1a7a0
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-08-28 21:19:13 -0300

    feat: add alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      '@/*': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@darkModeIcons': path.resolve(__dirname, './public/icons/dark'),
+      '@lightModeIcons': path.resolve(__dirname, './public/icons/light'),
+    }
+  }
 })
